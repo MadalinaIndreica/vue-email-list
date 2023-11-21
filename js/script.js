e0934213eee2227b275b43e07e66d32051aa3e7c
MadalinaIndreica <madalinaindreicamaddy4@gmail.com>
2023-11-21 14:55:48 +0100

    add: random emails added

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            
+            myEmails : [],
+
+        }
+
+    },
+
+    methods :{
+        generateEmail (){
+            for (i = 0; i < 10; i++){
+                 axios
+                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((resp) => {
+                    console.log(resp.data.response);
+                    this.myEmails.push(resp.data.response);
+                 })
+                }
+        }
+
+    }
+
+
+
+
+}).mount("#app");
